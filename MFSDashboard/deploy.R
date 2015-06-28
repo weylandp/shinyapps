1e3852525aa95be7cef0a4db64604026f07f13f4
Phil Weyland <phil.weyland@gmail.com>
2015-06-28 15:27:22 -0700

    Remove Deploy

--- MFSDashboard/deploy.R
+++
@@ -1,6 +0,0 @@
-library(RODBC)
-library(shiny)
-library(shinyapps)
-shinyapps::setAccountInfo(name='wdfw', token='FAA9F62F4EE9FF3ED8015BC6D399B0BD', secret='P/p+NZNX3avKX3gpq+0RI37QnYQvbxXofk/FRBDp')
-
-deployApp("C:/data/R Projects/ShinyApps/MFSDashboard", account = "wdfw", appName = "MFSReporting")
