124a877bf2aa64c34dfdc93774e897b381c99f39
weylandp <phil.weyland@gmail.com>
2015-10-09 12:45:04 -0700

    Added example .csv files and deploy.r script

--- MarineSpatialPlanning/ui.r
+++ MarineSpatialPlanning/ui.r
@@ -1,21 +1,36 @@
 library(shinydashboard)
+library(shiny)
 library(shinyBS)
+library(rpivotTable)
+library(DT)
 dashboardPage(
-  dashboardHeader(title = "WDFW Marine Fish Science"),
+  dashboardHeader(title = "MSP"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard"),
+      selectInput('exampleInput', 'Select Example', c('Example 1', 'Example 2', 'Example 3', 'Example 4')),
       menuItem("MSP Demo", icon = icon("th"),
-               menuSubItem ("Data", tabName = "mspdata", icon = icon("table")),
-               menuSubItem ("Plots", tabName = "mspplots", icon = icon("line-chart")),
-               menuSubItem ("Pivot Table", tabName = "msppivot", icon = icon("table"))
+               menuSubItem ("Data", tabName = "mspdatatab", icon = icon("table")),
+               menuSubItem ("Plots", tabName = "mspplotstab", icon = icon("line-chart")),
+               menuSubItem ("Pivot Table", tabName = "msppivottab", icon = icon("table"))
               
       )
     )
   ),
   dashboardBody(
+    tags$head(tags$style(
+      type = 'text/css',
+      '#msppivotplot{ overflow-x: scroll; }'
+    )),
     tabItems(
-      tabItem(tabName= "mspdata")
+      tabItem(tabName= "mspdatatab",
+              fluidRow(
+                DT::dataTableOutput('mspdatatable'))),
+      tabItem(tabName= "msppivottab",
+              fluidRow(
+                rpivotTableOutput('msppivotplot', height="100%"))),
+      tabItem(tabName= "mspplotstab",
+              fluidRow(
+                plotOutput('mspplot', width='100%', height =550)))
     )
   )
 )
